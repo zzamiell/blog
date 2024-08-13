cc5f38639804472e94186e45a5f370aa56b0eb3c
abdaiii <anugrah.abdi@transtrack.id>
2024-08-13 11:09:33 +0700

    test

---
+++ resources/views/wordpress.blade.php
@@ -0,0 +1,15 @@
+<!-- resources/views/wordpress.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WordPress Integration</title>
+</head>
+
+<body>
+    <iframe src="https://blog-staging.transtrack.id/en/" style="width: 100%; height: 100vh; border: none;"></iframe>
+</body>
+
+</html>
